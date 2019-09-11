6779e7dc4698d5dd714bb878e93eb9eb2cfa2558
Brian Candler <b.candler@pobox.com>
2019-09-11 17:44:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# rsyslog to loki adapter
+
+This is an experimental plugin to be run under omprog in rsyslog.  It
+accepts batches of messages (in transactions) and forwards them to the
+[loki API](https://github.com/grafana/loki/blob/master/docs/loki/api.md).
+
+It requires messages to be in the following form:
+
+```
+yyyy-mm-ddTHH:MM:SS.ssssss-ZZ:ZZ {label="value",...} Message goes here
+```
+
+which can be built using an rsyslog template.
+
+## Example configuration
+
+```
+template(name="loki" type="list") {
+  property(name="timegenerated" dateformat="rfc3339" date.inUTC="on")
+  constant(value=" {job=\"rsyslog\"")
+  constant(value=",src_ip=\"")
+  property(name="fromhost-ip" format="json")
+  constant(value="\",hostname=\"")
+  property(name="hostname" format="json")
+  constant(value="\",facility=\"")
+  property(name="syslogfacility-text" format="json")
+  constant(value="\",severity=\"")
+  property(name="syslogseverity-text" format="json")
+  constant(value="\"} ")
+  # Already escaped if EscapecontrolCharactersOnReceive is on (default)
+  property(name="msg" controlcharacters="escape")
+  constant(value="\n")
+}
+
+action(type="omprog"
+  template="loki"
+  binary="/usr/local/sbin/omprog-loki.py"
+  action.resumeInterval="5"
+  confirmMessages="on"
+  useTransactions="on"
+  output="/tmp/loki.err"
+  #reportFailures="on"    # only in 8.38.0+
+  forceSingleInstance="on")
+```
+
+FIXME: with this config, rsyslog is sending one message per transaction
+
+## Caveats
+
+Errors in your template can send `**INVALID PROPERTY NAME**` without a
+newline, and the external prog will hang waiting for one.
+
+If you use `timereported` instead of `timegenerated`, beware that devices
+with wrong clocks may result in messages being thrown away by loki for being
+too old.
+
+Be careful with your label set: do not include any high-cardinality
+properties.  For example, `programname` is probably OK, but `syslogtag`
+(which includes the PID) probably is not.  This is because Loki creates a
+new timeseries for every distinct combination of label values seen.
+
+The message scanning in `omprog-loki.py` currently isn't quite right, and
+can be confused by label values which start with `}` followed by space.
+
+## Licence
+
+This work is released under GPLv3 (same as rsyslog itself)
---
+++ omprog-loki.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import functools
+import re
+import requests
+import sys
+
+# Include the trailing \n in these
+BEGIN_TRANSACTION_MARK = "BEGIN TRANSACTION\n"
+COMMIT_TRANSACTION_MARK = "COMMIT TRANSACTION\n"
+CONFIRM_MESSAGES = True
+
+PUSH_URL = "http://localhost:3100/api/prom/push"
+PUSH_OPT = {}
+
+DEBUG = sys.stderr
+#DEBUG = open("/tmp/debug.out", "a")
+
+# We need to flush on every print()
+print = functools.partial(print, flush=True)
+
+def flush(events):
+    """
+    Send a batch as a single HTTP POST.  If there
+    is a temporary error then return an error
+    message, so that rsyslogd will retry
+    """
+    data = {"streams": [
+        {
+            "labels": labels,
+            "entries": entries
+        } for labels, entries in events.items()
+    ]}
+    r = requests.post(PUSH_URL, json=data, **PUSH_OPT)
+    events.clear()
+    if r.ok:
+        return "OK"
+    err = "Loki error: code %r: %s" % (r.status_code, r.text.replace("\n", " "))
+    print(err, file=DEBUG)
+    if 400 <= r.status_code <= 499:
+        # We sent badly-formatted data to loki, no point retrying.
+        # Beware https://github.com/grafana/loki/issues/929 (now fixed)
+        return "OK"
+    # Tell rsyslog of the error, it will resend all events to us
+    return err
+
+in_transaction = False
+events = {}   # {labels: [{"ts":ts,"line":line}, ...]}
+
+print("Starting...", file=DEBUG)
+if CONFIRM_MESSAGES:
+    print("OK") # signal we are ready
+for line in sys.stdin:
+    #print(repr(line), file=DEBUG)
+    if line == BEGIN_TRANSACTION_MARK:
+        in_transaction = True
+        if CONFIRM_MESSAGES:
+            print("OK")
+        continue
+    if line == COMMIT_TRANSACTION_MARK:
+        in_transaction = False
+        res = flush(events)
+        if CONFIRM_MESSAGES:
+            print(res)
+        continue
+    # FIXME: this doesn't handle pathological cases like {foo="} "}
+    m = re.match(r'^([0-9T:.+-]+) (\{.+?[^\\]"\}) (.*)$', line)
+    if not m:
+        # This line is badly formatted: we don't want to receive it again
+        print("Invalid line: %r" % line, file=DEBUG)
+        if CONFIRM_MESSAGES:
+            print("DEFER_COMMIT" if in_transaction else "OK")
+        continue
+    ts, labels, line = m.group(1), m.group(2), m.group(3)
+    if labels not in events:
+        events[labels] = []
+    events[labels].append({"ts":ts, "line":line})
+    if in_transaction:
+        if CONFIRM_MESSAGES:
+            print("DEFER_COMMIT")
+        continue
+    # If transaction mode not in use, flush and acknowledge
+    # messages one by one.  This is not very efficient.
+    res = flush(events)
+    if CONFIRM_MESSAGES:
+        print(res)
